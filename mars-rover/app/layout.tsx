63e4669b2d458718bb22455463727423272aa98a
Stefan Ghenescu <Stefan.Ghenescu@softwire.com>
2025-07-11 16:37:48 +0300

    Add header for home page and custom font

--- mars-rover/app/layout.tsx
+++ mars-rover/app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./navigation";
 
 const inter = Inter({ subsets: ["latin"] });
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Mars Rover",
--- mars-rover/app/page.tsx
+++ mars-rover/app/page.tsx
@@ -1,4 +1,5 @@
 import Starfield from './starfield';
+import styles from './page.module.css'
 
 export default function Home() {
     return (
@@ -8,18 +9,11 @@ export default function Home() {
                     starCount={2500}
                     starCount={2500}
                     startColor="white"
-                    speedFactor={0.1}
+                    speedFactor={0.15}
                     star
                     backgroundColor="black"
                     />
-                <div className="container mx-auto px-4">
-                    <h1>Hello word</h1>
-                    <p>This is a content to make our page longer</p>
-                    <div className="w-full h-screen bg-green-300"></div>
-                    <p>
-                        Lorem Ipsum is simply dummy text ...
-                    </p>
-                </div>
+                    <h1 className={styles.header}>Mars Rover</h1>
             </div>
         </>
     );
